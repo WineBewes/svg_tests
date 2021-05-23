6698ccccdc69501fbc5deb5b8c3a9d73d5f1e6fc
WineBewes <wine.bewes@gmail.com>
2021-05-23 14:50:25 +0200

    play

---
+++ main.js
@@ -0,0 +1,17 @@
+const vp = document.getElementById('viewport');
+
+vp.addEventListener('click', (event) => {
+
+  //console.log(event);
+  /*
+  console.log('{layerX: ' + event.layerX + ', layerY: ' + event.layerY + '}');
+  console.log('{offsetX: ' + event.offsetX + ', offsetY: ' + event.offsetY + '}');
+  */
+
+});
+
+document.addEventListener('keyup', (event) => {
+
+  console.log(event);
+
+});
